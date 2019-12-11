233dce739bcd0192bfd85d84b5153f3f788337e4
Michel Eter <micoror1@gmail.com>
2019-12-11 02:01:06 -0800

    Some functions added

--- TermProject.R
+++ TermProject.R
@@ -1,7 +1,13 @@
 removeVars_plotter <- function () {
   attach(mtcars)
-  par(mfrow=c(2,1))
+  par(mfrow=c(2,2))
   
   plot(day1$mnth, day1$temp, main="Temperature by Month (2011-2012)", xlab = "Month", ylab = "Temperature (C)")
   plot(day1$season, day1$temp, main="Temperature by Season (2011-2012)", xlab = "Season", ylab = "Temperature (C)")
+  
+  plot(day1$temp, day1$tot, main="Bike Users vs Temperature (2011-2012)", xlab = "Temperature (C)", ylab = "Number of Users (per day)")
+}
+
+linearModel <- function() {
+  summary(lm())
 }
